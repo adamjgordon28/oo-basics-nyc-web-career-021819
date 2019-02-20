9db4de893257f8f2a32f822168218f6276ac1e8b
Adam Gordon <adamjgordon24@gmail.com>
2019-02-20 19:28:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,29 +8,8 @@ class Book
     @title
   end
   
-  def author=(author)
-    @author = author
-  end
+  attr_accessor :author, :page_count, :genre
   
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
